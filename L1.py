8fee69912b1b49117cf8b04d1e5e7c52260bd06f
Amulya958 <amulya.gupta.2556@gdgoenkarohini.edu.in>
2026-03-17 21:45:29 +0530

    Lesson 1

---
+++ L1.py
@@ -0,0 +1,19 @@
+print("Hey Hi how are you doing? Enter your name :- ")
+
+name=input("Enter your name: ")
+
+print("Welcome user", name)
+print("How are you? good/bad? ")
+
+response= input().lower()
+
+if response == "good" :
+    print("Wow thats good to hear! Great going , lets connect later!")
+
+elif response == "bad" :
+	print("Ohh sad to hear that , please try stress removal methods and I am here to help you always!")
+
+else:
+	print("Ohh its hard to put feelings away easily, so I understand completely!")
+
+print("Bye for right now, lets meet later !!!")
